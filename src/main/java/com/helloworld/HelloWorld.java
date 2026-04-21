2446ce037c7011bcc5f7b275eb725c921ceb766d
redblacklucy <yeddulanehareddy@gmail.com>
2026-04-21 15:17:39 +0530

    UC2: Added command line argument for name

--- src/main/java/com/helloworld/HelloWorld.java
+++ src/main/java/com/helloworld/HelloWorld.java
@@ -2,7 +2,12 @@ package com.helloworld;
 
 public class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("Hello World");
 
-}
+        if (args.length > 0) {
+            System.out.println("Hello " + args[0] + "!");
+        } else {
+            System.out.println("Hello World");
+        }
+
+    }
 }
