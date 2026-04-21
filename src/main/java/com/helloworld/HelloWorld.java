974e1dcaaa323f31ad89107c5dec8fcee01c5bb7
redblacklucy <yeddulanehareddy@gmail.com>
2026-04-21 16:00:04 +0530

    UC7: String.join for multiple command-line arguments

--- src/main/java/com/helloworld/HelloWorld.java
+++ src/main/java/com/helloworld/HelloWorld.java
@@ -7,17 +7,9 @@ public class HelloWorld {
             System.out.println("Hello, World!");
         } else {
 
-            StringBuilder result = new StringBuilder();
-
-            for (String name : args) {
-                result.append("Hello ").append(name).append("! ");
-            }
-
-
-            String output = result.toString().trim();
-            output = output.substring(0, output.length());
-
-            System.out.println(output);
+            String names = String.join(", ", args);
+            System.out.println("Hello " + names + "!");
         }
+
     }
 }
