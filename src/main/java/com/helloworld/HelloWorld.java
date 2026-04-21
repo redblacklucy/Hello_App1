db00f52c57e23c285029f864081eae766f3835cd
redblacklucy <yeddulanehareddy@gmail.com>
2026-04-21 15:47:29 +0530

    UC5: Enhanced for-each loop with default greeting

--- src/main/java/com/helloworld/HelloWorld.java
+++ src/main/java/com/helloworld/HelloWorld.java
@@ -2,7 +2,14 @@ package com.helloworld;
 
 public class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("Hello World");
 
-}
+        if (args.length == 0) {
+            System.out.println("Hello, World!");
+        } else {
+            for (String name : args) {
+                System.out.println("Hello " + name + "!");
+            }
+        }
+
+    }
 }
